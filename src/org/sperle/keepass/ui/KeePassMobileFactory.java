c75584c34b5443405002ec5a03386dfecdce2d57
Christoph <Christoph@b890fd34-737e-8241-86d3-67d139f95938>
2010-08-10 19:31:46 +0000

    Updated copyright information in source files

--- src/org/sperle/keepass/ui/KeePassMobileFactory.java
+++ src/org/sperle/keepass/ui/KeePassMobileFactory.java
@@ -1,3 +1,23 @@
+/*
+    Copyright (c) 2009-2010 Christoph Sperle <keepassmobile@gmail.com>
+    
+    This file is part of KeePassMobile.
+
+    KeePassMobile is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    KeePassMobile is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with KeePassMobile.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 package org.sperle.keepass.ui;
 
 import org.sperle.keepass.KeePassMobileIO;
