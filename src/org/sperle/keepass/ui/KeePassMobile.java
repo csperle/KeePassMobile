ef19763e622ccdbab0ea62bf95e2df50ec165bcc
Christoph <Christoph@b890fd34-737e-8241-86d3-67d139f95938>
2010-10-03 20:00:09 +0000

    Deleted obsolete dev/libs dir

--- src/org/sperle/keepass/ui/KeePassMobile.java
+++ src/org/sperle/keepass/ui/KeePassMobile.java
@@ -55,7 +55,7 @@ public class KeePassMobile implements Application {
     public static final String VERSION = "V0.9";
     public static final String COPYRIGHT = "Copyright (c) 2010";
     public static final String ME = "Christoph Sperle";
-    public static final String LICENSE = "Copyright (c) 2010 Christoph Sperle <keepassmobile@gmail.com>\n\n" +
+    public static final String LICENSE = "Copyright (c) 2009-2010 Christoph Sperle <keepassmobile@gmail.com>\n\n" +
         "KeePassMobile is free software: you can redistribute it and/or modify it under the terms of the GNU General " +
         "Public License as published by the Free Software Foundation, either version 3 of the License, or (at your " +
         "option) any later version.\n\n" +
